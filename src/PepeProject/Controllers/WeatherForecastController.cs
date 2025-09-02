414ac718c130ed1b31e3a3407eee739cce594a10
Wogih <Wogih10@gmail.com>
2025-09-02 20:55:48 +0300

    Add API project

---
+++ src/PepeProject/Controllers/WeatherForecastController.cs
@@ -0,0 +1,112 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PepeProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static List<string> Summaries = new()
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(int? sortIndex)
+        {
+            if (sortIndex == null)
+            {
+                return Ok(Summaries);
+            }
+
+            if (sortIndex != 1 && sortIndex != -1)
+            {
+                return BadRequest("Некорректное значение параметра sortStrategy");
+            }
+
+            var sortedList = Summaries.ToList();
+
+            if (sortIndex == 1)
+            {
+                sortedList.Sort();
+            }
+            else if (sortIndex == -1)
+            {
+                sortedList.Sort();
+                sortedList.Reverse();
+            }
+
+            return Ok(sortedList);
+        }
+
+        [HttpGet("{index:int}")]
+        public IActionResult GetByIndex(int index)
+        {
+            if (index < 0 || index >= Summaries.Count)
+            {
+                return NotFound($"Элемент с индексом {index} не найден");
+            }
+
+            return Ok(Summaries[index]);
+        }
+
+        [HttpGet("find-by-name")]
+        public IActionResult FindByName(string? name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest("Имя не может быть пустым");
+            }
+
+            return Ok(Summaries.Count(s => s == name));
+        }
+
+        [HttpPost]
+        public IActionResult Add(string? name)
+        {
+            if (string.IsNullOrEmpty(name))
+            { 
+                return BadRequest("Имя не может быть пустым");
+            }
+
+            Summaries.Add(name);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(int index, string? name)
+        {
+            if (index < 0 || index >= Summaries.Count)
+            {
+                return BadRequest("Такой индекс неверный");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest("Имя не может быть пустым");
+            }
+
+            Summaries[index] = name;
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int index)
+        {
+            if (index < 0 || index >= Summaries.Count)
+            {
+                return BadRequest("Данный индекс неверный");
+            }
+
+            Summaries.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
