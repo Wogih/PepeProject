c5565aa114a8d921772921bd1811d51072ef328b
Wogih <Wogih10@gmail.com>
2025-10-19 21:45:23 +0300

    dotnet format

--- src/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ src/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f8445e4d4d34337a7cbb06088b7ab07d798034e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fd476e7f593010cabd72456dad42bd63ea666c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -76,7 +76,7 @@ namespace PepeProject
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
-            
+
                 var context = services.GetRequiredService<MisContext>();
                 context.Database.Migrate();
             }
